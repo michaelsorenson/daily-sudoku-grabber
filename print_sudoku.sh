593b1981bfeeca86d8bf33803d14b362fb56d671
Michael Sorenson <michaelsorenson427@gmail.com>
2022-04-05 09:15:58 -0700

    Removed gpa's puzzle ;(

--- print_sudoku.sh
+++ print_sudoku.sh
@@ -6,5 +6,5 @@ pdflatex gma_sudoku_puzzle.tex
 pdflatex gpa_sudoku_puzzle.tex
 lp ang_mon_sudoku_puzzle.pdf
 lp gma_sudoku_puzzle.pdf
-lp gpa_sudoku_puzzle.pdf
+#lp gpa_sudoku_puzzle.pdf
 rm *sudoku_puzzle.*

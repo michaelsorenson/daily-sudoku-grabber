ab9ddbed3769064002a454f89d15e74adb8a7f45
Michael Sorenson <michaelsorenson427@gmail.com>
2022-07-17 13:28:37 -0700

    Update print_sudoku.sh

--- print_sudoku.sh
+++ print_sudoku.sh
@@ -3,8 +3,10 @@ git pull origin master
 python3 dailySudokuGrabber.py
 pdflatex ang_mon_sudoku_puzzle.tex
 pdflatex gma_sudoku_puzzle.tex
+pdflatex gma_friend_sudoku_puzzle.tex
 pdflatex gpa_sudoku_puzzle.tex
-lp ang_mon_sudoku_puzzle.pdf
+#lp ang_mon_sudoku_puzzle.pdf
 lp gma_sudoku_puzzle.pdf
 #lp gpa_sudoku_puzzle.pdf
+lp gma_friend_sudoku_puzzle.pdf
 rm *sudoku_puzzle.*
